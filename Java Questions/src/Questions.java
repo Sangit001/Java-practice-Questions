58816a315e8d608624df320089b5dbe42c0d5fbb
Sangit001 <sangit1543@gmail.com>
2026-01-15 17:56:32 +0545

    Done 2 Question

---
+++ Java Questions/src/Questions.java
@@ -0,0 +1,106 @@
+
+
+
+
+//Write a Java program to create a class called "Person" with a name and age attribute.
+// Create two instances of the "Person" class, set their attributes using the constructor, and print their name and age.
+//
+//class Person{
+//    String name;
+//    int age;
+//    Person(String name, int age){
+//        this.name = name;
+//        this.age = age;
+//
+//    }
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public int getAge(){
+//        return age;
+//    }
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//    public void setAge(int age){
+//        this.age = age;
+//    }
+//}
+//
+//
+//
+//public class Questions {
+//public static void main(String[] args) {
+//
+//    Person person1 = new Person("sangit", 21);
+//    Person person2 = new Person("david", 22);
+//
+//    System.out.println(person1.getName());
+//    System.out.println(person1.getAge());
+//    System.out.println(person2.getName());
+//    System.out.println(person2.getAge());
+//
+//
+//}
+//}
+
+
+//2. Write a Java program to create a class called "Dog" with a name and breed attribute.
+// Create two instances of the "Dog" class, set their attributes using the constructor and modify the
+// attributes using the setter methods and print the updated values.
+
+/*
+
+class Dog{
+    private String name;
+    private String breed;
+    Dog(String name, String breed){
+        this.name = name;
+        this.breed = breed;
+        }
+
+
+    public String getName(){
+        return name;
+    }
+    public String getBreed(){
+        return breed;
+    }
+
+    public void setName(String name){
+        this.name  = name;
+    }
+    public void setBreed(String breed){
+        this.breed = breed;
+    }
+}
+
+public class Questions {
+
+    public static void main(String[] args){
+
+        Dog dog1 = new Dog("puppu", "german");
+        Dog dog2 = new Dog("luppy", "japanese");
+
+        System.out.println("First dog name is: "+dog1.getName());
+        System.out.println("First dog breed is: "+dog1.getBreed());
+        System.out.println("Second dog name is: "+dog2.getName());
+        System.out.println("Second dog breed is: "+dog2.getBreed());
+
+        dog1.setName("Oreo");
+        dog1.setBreed("unknown");
+        dog2.setName("Dorio");
+        dog2.setBreed("German");
+
+        System.out.println("First dog name is: "+dog1.getName());
+        System.out.println("First dog breed is: "+dog1.getBreed());
+        System.out.println("Second dog name is: "+dog2.getName());
+        System.out.println("Second dog breed is: "+dog2.getBreed());
+    }
+}
+
+*/
+
+
